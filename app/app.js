68b07a8e6b711af3d4f79e82664cac7a3cb147a4
Daniele Ricci <daniele@casaricci.it>
2018-02-02 00:50:57 +0100

    Very simple dashboard with thermostat control
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/app.js
+++ app/app.js
@@ -2,11 +2,26 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.dashboard'
+    'ngRoute',
+    'myApp.dashboard'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+config(['$locationProvider', '$routeProvider', '$httpProvider', function($locationProvider, $routeProvider, $httpProvider) {
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/dashboard'});
+    /** TODO for when we'll have login
+    $httpProvider.interceptors.push(function($q, dependency1, dependency2) {
+        return {
+            'request': function(config) {
+                return config;
+            },
+
+            'response': function(response) {
+                // TODO
+                return response;
+            }
+        };
+    });
+     */
+
+    $routeProvider.otherwise({redirectTo: '/dashboard'});
 }]);
--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,14 +1,72 @@
 'use strict';
 
-angular.module('myApp.dashboard', ['ngRoute'])
+angular.module('myApp.dashboard', ['ngRoute', 'ngHTTPPoll'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/dashboard', {
-    templateUrl: 'dashboard/dashboard.html',
-    controller: 'DashboardCtrl'
-  });
+    $routeProvider.when('/dashboard', {
+        templateUrl: 'dashboard/dashboard.html',
+        controller: 'DashboardCtrl'
+    });
 }])
 
-.controller('DashboardCtrl', [function() {
+.controller('DashboardCtrl', ['$scope', 'dashboardStatusService', function($scope, dashboardStatusService) {
+    $scope.thermostatDial = new thermostatDial(document.getElementById('dashboard-thermostat'), {
+        onSetTargetTemperature: function (targetTemperature) {
+            // TODO alter pipeline
+            console.log(targetTemperature);
+        }
+    });
+    dashboardStatusService.pollStatus($scope.thermostatDial);
+}])
 
+.factory('dashboardStatusService', ['$http', '$httpoll', function ($http, $httpoll) {
+    return {
+        // FIXME hard-coding all the way!!!
+
+        pollStatus: function(dial) {
+            // target temperature from active pipeline
+            $http({
+               url: '/api/pipelines/active',
+            })
+            .then(function(res) {
+                if (res.data && res.data.behaviors && res.data.behaviors[0] && res.data.behaviors[0].config && res.data.behaviors[0].config.target_temperature) {
+                    dial.target_temperature = res.data.behaviors[0].config.target_temperature;
+                }
+            });
+
+            $httpoll({
+                url: '/api/devices/status/home_boiler',
+                delay: 1000
+                // TODO other params?
+            })
+            .then(function(res) {
+                if (res.data && res.data.status && res.data.status.enabled) {
+                    dial.hvac_state = 'heating';
+                }
+                else {
+                    dial.hvac_state = 'off';
+                }
+            })
+            .catch(function(res) {
+                console.log(res);
+            });
+
+            $httpoll({
+                url: '/api/sensors/reading/temp_core',
+                delay: 1000
+                // TODO other params?
+            })
+            .then(function(res) {
+                if (res.data && res.data.type === 'temperature' && res.data.value) {
+                    dial.ambient_temperature = res.data.value;
+                }
+                else {
+                    dial.ambient_temperature = -1;
+                }
+            })
+            .catch(function(res) {
+                console.log(res);
+            });
+        }
+    }
 }]);
