96b5d2fa4aa8a1bee90cac28a3ac8835b2c3ae7d
Daniele Ricci <daniele@casaricci.it>
2018-02-02 01:09:32 +0100

    Automatic update of dashboard values
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -32,40 +32,37 @@ angular.module('myApp.dashboard', ['ngRoute', 'ngHTTPPoll'])
                 if (res.data && res.data.behaviors && res.data.behaviors[0] && res.data.behaviors[0].config && res.data.behaviors[0].config.target_temperature) {
                     dial.target_temperature = res.data.behaviors[0].config.target_temperature;
                 }
+            })
+            .catch(function(res) {
+                console.log(res);
             });
 
             $httpoll({
                 url: '/api/devices/status/home_boiler',
-                delay: 1000
+                delay: 1000,
+                until: function(response, config, state, actions) {
+                    if (response.data && response.data.status && response.data.status.enabled) {
+                        dial.hvac_state = 'heating';
+                    }
+                    else {
+                        dial.hvac_state = 'off';
+                    }
+                }
                 // TODO other params?
-            })
-            .then(function(res) {
-                if (res.data && res.data.status && res.data.status.enabled) {
-                    dial.hvac_state = 'heating';
-                }
-                else {
-                    dial.hvac_state = 'off';
-                }
-            })
-            .catch(function(res) {
-                console.log(res);
             });
 
             $httpoll({
                 url: '/api/sensors/reading/temp_core',
-                delay: 1000
+                delay: 1000,
+                until: function(response, config, state, actions) {
+                    if (response.data && response.data.type === 'temperature' && response.data.value) {
+                        dial.ambient_temperature = response.data.value;
+                    }
+                    else {
+                        dial.ambient_temperature = -1;
+                    }
+                }
                 // TODO other params?
-            })
-            .then(function(res) {
-                if (res.data && res.data.type === 'temperature' && res.data.value) {
-                    dial.ambient_temperature = res.data.value;
-                }
-                else {
-                    dial.ambient_temperature = -1;
-                }
-            })
-            .catch(function(res) {
-                console.log(res);
             });
         }
     }
