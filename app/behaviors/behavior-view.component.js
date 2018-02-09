b102488587de912a393eb3ba5666638968c72fe6
Daniele Ricci <daniele@casaricci.it>
2018-02-09 22:04:09 +0100

    Force temperature template as a component
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/behaviors/behavior-view.component.js
+++ app/behaviors/behavior-view.component.js
@@ -7,36 +7,8 @@ angular.module('app.behavior-view')
     bindings: {
         behavior: '<'
     },
-    controller: ['$scope', 'Pipelines', function BehaviorViewController($scope, Pipelines) {
+    controller: ['$scope', function BehaviorViewController($scope) {
         let ctrl = this;
-
-        $scope.successInputGroup = function(buttonId) {
-            return function() {
-                angular.element(document.getElementById(buttonId))
-                    .removeClass('btn-outline-secondary')
-                    .addClass('btn-success')
-                    .find('i')
-                    .removeClass('fa-save')
-                    .addClass('fa-check');
-            };
-        };
-
-        $scope.restoreInputGroup = function(buttonId) {
-            angular.element(document.getElementById(buttonId))
-                .removeClass('btn-success')
-                .addClass('btn-outline-secondary')
-                .find('i')
-                .removeClass('fa-check')
-                .addClass('fa-save');
-        };
-
-        $scope.save = function(callback) {
-            console.log(ctrl.behavior);
-            Pipelines.active_set_config(ctrl.behavior.order, ctrl.behavior.config)
-            .then(function(res) {
-                callback(res);
-            });
-        };
 
         ctrl.$onInit = function() {
             // TODO
---
+++ app/behaviors/force-temperature.component.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('app.behavior-view')
+
+.component('forceTemperatureBehavior', {
+    templateUrl: 'behaviors/force-temperature.template.html',
+    bindings: {
+        behavior: '<'
+    },
+    controller: ['$scope', 'Pipelines', function BehaviorViewController($scope, Pipelines) {
+        let ctrl = this;
+
+        $scope.successInputGroup = function(buttonId) {
+            return function() {
+                angular.element(document.getElementById(buttonId))
+                    .removeClass('btn-outline-secondary')
+                    .addClass('btn-success')
+                    .find('i')
+                    .removeClass('fa-save')
+                    .addClass('fa-check');
+            };
+        };
+
+        $scope.restoreInputGroup = function(buttonId) {
+            angular.element(document.getElementById(buttonId))
+                .removeClass('btn-success')
+                .addClass('btn-outline-secondary')
+                .find('i')
+                .removeClass('fa-check')
+                .addClass('fa-save');
+        };
+
+        $scope.save = function(callback) {
+            Pipelines.active_set_config(ctrl.behavior.order, ctrl.behavior.config)
+                .then(function(res) {
+                    callback(res);
+                });
+        };
+    }]
+});
