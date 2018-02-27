95510b457c5460dcf9202228b31f5d254dc3265c
Daniele Ricci <daniele@casaricci.it>
2018-02-27 15:50:19 +0100

    Delete old service worker caches
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/sw.js
+++ app/sw.js
@@ -29,6 +29,20 @@ self.addEventListener('install', function(event) {
     );
 });
 
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (CACHE_NAME !== cacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', function(event) {
     // do not cache requests to APIs
     const requestUrl = new URL(event.request.url);
