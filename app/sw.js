2df260ad651d9a55680d39021f685ac3cfae7a53
Daniele Ricci <daniele@casaricci.it>
2018-03-03 19:53:51 +0100

    Service worker version dump
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/sw.js
+++ app/sw.js
@@ -2,7 +2,7 @@
 // inspired by https://developers.google.com/web/fundamentals/primers/service-workers/
 // It will cache everything that the browser will request
 
-const CACHE_NAME = 'thermorasp-cache-v1';
+const CACHE_NAME = 'thermorasp-cache-v2';
 const urlsToCache = [
     '/',
     '/manifest.json',
