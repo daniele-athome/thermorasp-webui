6420386a8c60206974e3af3d989ec9e82c974267
Daniele Ricci <daniele@casaricci.it>
2018-12-16 00:00:00 +0100

    Using FullCalendar for showing the schedule
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- src/app/routes/schedules/schedule-list/schedule-list.component.ts
+++ src/app/routes/schedules/schedule-list/schedule-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as $ from 'jquery';
+import 'fullcalendar';
+import 'fullcalendar-scheduler';
+import * as moment from 'moment';
+import { ScheduleService } from "../../../core/services";
+import { Schedule, ScheduleBehavior } from "../../../core/models";
+import { AssertionError } from "assert";
+import { getTemperatureColor } from "../../../shared";
 
 @Component({
   selector: 'app-schedule-list',
@@ -7,9 +15,157 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleListComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('schedule')
+  scheduleElement: ElementRef;
+
+  private calendar$: JQuery;
+
+  constructor(private scheduleService: ScheduleService) { }
 
   ngOnInit() {
+    this.calendar$ = $(this.scheduleElement.nativeElement);
+    this.calendar$.fullCalendar({
+      slotLabelFormat: 'H:mm',
+      timeFormat: 'H:mm',
+      editable: true,
+      selectable: true,
+      nowIndicator: true,
+      header: {
+        'left': 'title',
+        'center': '',
+        'right': '',
+      },
+      titleFormat: '[...]',
+      height: 'auto',
+      defaultDate: '2000-01-01',
+      defaultView: 'timelineDay',
+      views: {
+        timelineWeek: {
+          type: 'timeline',
+          duration: { days: 1 }
+        }
+      },
+      //slotWidth: 50,
+      resourceAreaWidth: 100,
+      resourceLabelText: 'Days',
+      resources: [
+        {id: '1', title: 'Monday'},
+        {id: '2', title: 'Tuesday'},
+        {id: '3', title: 'Wednesday'},
+        {id: '4', title: 'Thursday'},
+        {id: '5', title: 'Friday'},
+        {id: '6', title: 'Saturday'},
+        {id: '7', title: 'Sunday'},
+      ],
+      select: this.selectRange,
+      schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source'
+    });
+
+    // time to fill it!
+    this.loadSchedule();
+  }
+
+  private loadSchedule() {
+    // TODO what if there is no active schedule?
+    this.scheduleService.active().subscribe(
+      (schedule: Schedule) => {
+        this.calendar$.fullCalendar('option', 'titleFormat', '[' + schedule.name + ']');
+
+        const events = [];
+        schedule.behaviors.forEach(
+          (behavior: ScheduleBehavior) => {
+            events.push(...ScheduleListComponent.createEvents(behavior));
+          }
+        );
+
+        console.log(events);
+        this.calendar$.fullCalendar('removeEventSources', null);
+        this.calendar$.fullCalendar('addEventSource', events);
+      }
+    );
+  }
+
+  /** Very crappy algorithm to create FullCalendar events for a behavior. */
+  private static createEvents(behavior: ScheduleBehavior): any[] {
+    const events = [];
+    const diff_mins = behavior.end_time - behavior.start_time;
+
+    const days_span = Math.ceil(diff_mins / 60 / 24);
+    console.log("Days spanning: " + days_span);
+    const first_day = Math.floor(behavior.start_time / 60 / 24) + 1;
+    const last_day = first_day + days_span;
+
+    for (let day_index = first_day; day_index < last_day; day_index++) {
+      console.log("Day " + day_index);
+      if (day_index == first_day && day_index < (last_day - 1)) {
+        console.log("BEGIN-MIDNIGHT");
+        const start = moment('2000-01-01 ' +
+          Math.trunc((behavior.start_time - ((first_day - 1) * 60 * 24)) / 60) + ':' +
+          behavior.start_time % 60, 'YYYY-MM-DD H:m');
+        const end = moment('2000-01-02 00:00', 'YYYY-MM-DD H:m');
+        events.push({
+          title: behavior.config['target_temperature'],
+          backgroundColor: getTemperatureColor(behavior.config['target_temperature']),
+          textColor: 'white',
+          resourceId: String(day_index),
+          start: start,
+          end: end,
+        });
+      }
+      else if (day_index > first_day && day_index < (last_day - 1)) {
+        console.log("FULL-DAY");
+        const start = moment('2000-01-01 00:00', 'YYYY-MM-DD H:m');
+        const end = moment('2000-01-02 00:00', 'YYYY-MM-DD H:m');
+        events.push({
+          title: behavior.config['target_temperature'],
+          backgroundColor: getTemperatureColor(behavior.config['target_temperature']),
+          textColor: 'white',
+          resourceId: String(day_index),
+          start: start,
+          end: end,
+        });
+      }
+      else if (day_index > first_day && day_index == (last_day - 1)) {
+        console.log("MIDNIGHT-END");
+        const start = moment('2000-01-01 00:00', 'YYYY-MM-DD H:m');
+        const end = moment('2000-01-01 ' +
+          Math.trunc((behavior.end_time - ((last_day - 2) * 60 * 24)) / 60) + ':' +
+          behavior.end_time % 60, 'YYYY-MM-DD H:m');
+        events.push({
+          title: behavior.config['target_temperature'],
+          backgroundColor: getTemperatureColor(behavior.config['target_temperature']),
+          textColor: 'white',
+          resourceId: String(day_index),
+          start: start,
+          end: end,
+        });
+      }
+      else if (day_index == first_day && day_index == (last_day - 1)) {
+        console.log("MID-DAY");
+        const start = moment('2000-01-01 ' +
+          Math.trunc((behavior.start_time - ((first_day - 1) * 60 * 24)) / 60) + ':' +
+          behavior.start_time % 60, 'YYYY-MM-DD H:m');
+        const end = moment('2000-01-01 ' +
+          Math.trunc((behavior.end_time - ((last_day - 2) * 60 * 24)) / 60) + ':' +
+          behavior.end_time % 60, 'YYYY-MM-DD H:m');
+        events.push({
+          title: behavior.config['target_temperature'],
+          backgroundColor: getTemperatureColor(behavior.config['target_temperature']),
+          resourceId: String(day_index),
+          start: start,
+          end: end,
+        });
+      }
+      else {
+        throw new AssertionError({message: 'NOOOOOOOOOOOOOOOOO!!!!!'});
+      }
+    }
+
+    return events;
+  }
+
+  selectRange(start: moment.Moment, end: moment.Moment, jsEvent: MouseEvent, view, resource) {
+    console.log({start: start, end: end, jsEvent: jsEvent, view: view, resource: resource})
   }
 
 }
--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,5 +1,6 @@
 
-import { getHours, getMinutes, getDay, endOfDay } from 'date-fns'
+import { getHours, getMinutes, getDay, endOfDay } from 'date-fns';
+import { ColorGradient } from './color-gradient';
 
 export function getCurrentMinute(): number {
   return getMinuteInDay(new Date());
@@ -13,4 +14,13 @@ function getMinuteInDay(date: Date): number {
   return ((getDay(date) - 1) * 24 * 60) +
          (getHours(date) * 60) +
           getMinutes(date);
+}
+
+// gradient will be derived from this interval
+const tempInterval = [15, 25];
+const tempGradient = new ColorGradient(["#00f", "#f23700"]);
+
+export function getTemperatureColor(temp: number): string {
+  const percent = (temp - tempInterval[0]) / (tempInterval[1] - tempInterval[0]) * 100;
+  return tempGradient.getHexColorAtPercent(percent);
 }
