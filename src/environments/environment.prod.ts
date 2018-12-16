bc62a2cee016fb4ddf6ddfe5c7d14ec37b46ce48
Daniele Ricci <daniele@casaricci.it>
2018-12-16 21:57:03 +0100

    Presumed production configuration
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   api_url: '/api',
-  mqtt_path: '/mqtt',
+  mqtt_host: window.location.hostname,
+  mqtt_port: Number(window.location.port),
+  mqtt_path: '/mqtt/',
   sensor_validity: 600
 };
