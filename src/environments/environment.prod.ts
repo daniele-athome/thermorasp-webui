f7ffda6777b5de2eb672025d733c9343b3a8b84b
Daniele Ricci <daniele@casaricci.it>
2018-12-22 19:49:22 +0100

    Default MQTT port
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   api_url: '/api',
   mqtt_host: window.location.hostname,
-  mqtt_port: Number(window.location.port),
+  mqtt_port: window.location.port ? Number(window.location.port) : 80,
   mqtt_path: '/mqtt/',
   sensor_validity: 600
 };
